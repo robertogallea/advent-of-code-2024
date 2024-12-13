ab145a7999511fe88dc4ad03ece25e83b893eac0
Roberto Gallea <write@robertogallea.com>
2024-12-13 13:10:57 +0100

    Minor refactorings

--- src/Day13.php
+++ src/Day13.php
@@ -18,13 +18,15 @@ class Day13
 
     private function parseInput(string $input, bool $withCorrection = false): array
     {
+        $result = array();
+
         $machines = explode(PHP_EOL.PHP_EOL, $input);
 
         foreach ($machines as $machine) {
             $rows = explode(PHP_EOL, $machine);
             preg_match("/X\+(\d+), Y\+(\d+)/", $rows[0], $matchesA);
             preg_match("/X\+(\d+), Y\+(\d+)/", $rows[1], $matchesB);
-            preg_match("/X\=(\d+), Y\=(\d+)/", $rows[2], $matchesP);
+            preg_match("/X=(\d+), Y=(\d+)/", $rows[2], $matchesP);
 
             $result[] = [
                 'A' => [
@@ -41,13 +43,13 @@ class Day13
                 ]
             ];
         }
+
         return $result;
     }
 
     /**
      * @param  array  $machines
-     * @param  float|int  $result
-     * @return float|int
+     * @return float
      */
     public function findPrizes(array $machines): int
     {
@@ -59,7 +61,7 @@ class Day13
                 (($machine['B']['Y'] * $machine['A']['X']) - ($machine['B']['X'] * $machine['A']['Y']));
             $moveY = (($machine['A']['X'] * $machine['B']['X'] * $machine['P']['Y']) - ($machine['P']['X'] * $machine['B']['X'] * $machine['A']['Y'])) /
                 ($machine['B']['X'] * (($machine['B']['Y'] * $machine['A']['X']) - ($machine['B']['X'] * $machine['A']['Y'])));
-            
+
             if ((intval($moveX) == $moveX) && ((intval($moveY) == $moveY))) {
                 $result += 3 * $moveX + $moveY;
             }
